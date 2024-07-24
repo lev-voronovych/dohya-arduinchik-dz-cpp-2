ffcc4b80ee82459e923eebcb91c14f0f0cf1d700
lev-voronovych <vlev0760@gmail.com>
2024-07-24 20:09:46 +0300

    шо

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+
+    switch (a) {
+        case 1:
+          cin >> a;
+            break;
+        case 2:
+            return 0;
+        default:
+            cout << "ви даун!!!!" << endl;
+            return 0;
+    }
+}
+
+
